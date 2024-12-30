21f1858692b1ac5dd333ee82b19865578a4f1cba
apmanager <121115177+apmanager001@users.noreply.github.com>
2024-12-30 16:53:41 -0500

    updated projects page and added npm project

--- src/app/portfolio/comp/projects.jsx
+++ src/app/portfolio/comp/projects.jsx
@@ -2,154 +2,107 @@ import React from "react";
 import Link from "next/link";
 
 const ProjectSection = () => {
+
+  const projects = [
+    {
+      title: "NPM Package: simple-react-heatcalendar",
+      href: "https://www.npmjs.com/package/simple-react-heatcalendar",
+      imgSrc: "/projects/npm.jpg",
+      badges: [
+        "React",
+        "NPM",
+        <img
+          src="https://img.shields.io/npm/dw/simple-react-heatcalendar"
+          alt="NPM Downloads"
+        />,
+      ],
+      description: "This is a public npm package that allows users to ",
+    },
+    {
+      title: "Quilted Chronicles",
+      href: "https://quiltedchronicles.org",
+      imgSrc: "/projects/adventure.jpg",
+      badges: ["React", "Mongo DB", "Express", "Node"],
+      description:
+        "This project was a group effort. I worked on the frontend and someone else worked on backend. This project gave me experience working and communicating with someone else. It was important to communicate what data was needed from backend for the site to work.",
+    },
+    {
+      title: "My Trip Map",
+      href: "https://mytripmaps200.netlify.app/",
+      imgSrc: "/projects/tripmap.jpg",
+      badges: ["React", "Mongo DB", "Express", "Map Leaflet"],
+      description:
+        "This project was great learning how map leaflets work. The idea of the site is users can upload photos and it uses metadata to find the location and pin that location on the map. I also added using an API to find the top 10 closest Points of interest add it on the map.",
+    },
+    {
+      title: "LM Treasure Trove",
+      href: "https://lmtreasuretrove.com",
+      imgSrc: "/projects/lmtreasure.jpg",
+      badges: ["React", "Mongo DB", "Express", "Stripe API"],
+      description:
+        "This project was built specific for a client and their currently ran business. They needed a working cart that adds items. When the client is ready to checkout the payment is processed through Stripe. Fully functional Store.",
+    },
+    {
+      title: "Coup Group",
+      href: "https://www.coupgroup.com",
+      imgSrc: "/projects/coup.jpg",
+      badges: ["React", "Mongo DB", "Express", "Node"],
+      description:
+        "This project was built for a client and changed to a SAAS. I had close communication with the client on what was needed to help manage their clients. Specifically, what information needs to be stored and explaining how secure their clients data is.",
+    },
+    {
+      title: "Habiting",
+      href: "https://habiting.netlify.app/",
+      imgSrc: "/projects/habiting.jpg",
+      badges: ["React", "Supabase", "Oauth"],
+      description:
+        "This project was a passion project for me. I wanted to build a project that allowed me to track habits that I want to build. It allows users to focus on a few core habits to build that muscle to continue consistency.",
+    },
+  ];
+
   return (
     <div className="flex flex-col my-4">
       <div className="flex flex-wrap justify-center py-10 gap-8 max-w-screen-lg mx-auto">
-        <div className="flex flex-col gap-2 w-3/4 md:w-80 border border-gray-600 hover:border-gray-400 rounded-2xl p-10 shadow-2xl">
-          <Link
-            href="https://quiltedchronicles.org"
-            target="_blank"
-            className="flex flex-col items-center"
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="flex flex-col gap-2 w-3/4 md:w-80 border border-gray-600 hover:border-gray-400 rounded-2xl shadow-2xl"
           >
-            <h2 className="text-center text-white text-xl">
-              Quilted Chronicles
-            </h2>
-            <img
-              src="/projects/adventure.jpg"
-              className="h-32 border border-gray-600 my-2"
-              alt="project image"
-            />
-            <div className="w-full flex flex-col gap-2 px-2">
-              <div className="flex gap-2 flex-wrap justify-center">
-                <div className="badge badge-primary">React</div>
-                <div className="badge badge-primary">Mongo DB</div>
-                <div className="badge badge-primary">Express</div>
-                <div className="badge badge-primary">Node</div>
+            <Link
+              href={project.href}
+              target="_blank"
+              className="flex flex-col items-center"
+            >
+              <div className="w-full">
+                <img
+                  src={project.imgSrc}
+                  className="h-36 w-full border-b-2 border-gray-400 mb-2 rounded-t-2xl"
+                  alt="project image"
+                />
               </div>
-              <div className="text-sm text-gray-500 ">
-                This project was a group effort. I worked on the frontend and
-                someone else worked on backend. This project gave me experience
-                working and communicating with someone else. It was important to
-                communicate what data was needed from backend for the site to
-                work.
+              <div className="w-full flex flex-col gap-2 px-10 pb-2">
+                <div className="flex gap-2 flex-wrap justify-center">
+                  {project.badges.map((badge, index) =>
+                    typeof badge === "string" ? (
+                      <div key={index} className="badge badge-primary">
+                        {" "}
+                        {badge}{" "}
+                      </div>
+                    ) : (
+                      <div key={index}> {badge} </div>
+                    )
+                  )}
+                </div>
+                <h2 className="text-center text-white text-xl">
+                  {project.title}
+                </h2>
+                <div className="text-sm text-gray-500 ">
+                  {project.description}
+                </div>
               </div>
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col gap-2 w-3/4 md:w-80 border border-gray-600 hover:border-gray-400 rounded-2xl p-10 shadow-2xl">
-          <Link
-            href="https://mytripmaps200.netlify.app/"
-            target="_blank"
-            className="flex flex-col items-center"
-          >
-            <h2 className="text-center text-white text-xl">My Trip Map</h2>
-            <img
-              src="/projects/tripmap.jpg"
-              className="h-32 border border-x-gray-600 border-y-gray-600 my-2"
-              alt="project image"
-            />
-            <div className="w-full flex flex-col gap-2 px-2">
-              <div className="flex flex-wrap gap-2 justify-center">
-                <div className="badge badge-primary">React</div>
-                <div className="badge badge-primary">Mongo DB</div>
-                <div className="badge badge-primary">Express</div>
-                <div className="badge badge-primary">Map Leaflet</div>
-              </div>
-              <div className="text-sm text-gray-500 ">
-                This project was great learning how map leaflets work. The idea
-                of the site is users can uplaod photos and it uses metadata to
-                find the location and pin that location on the map. I also added
-                using an API to find the top 10 closest Points of interest add
-                it on the map.
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col gap-2 w-3/4 md:w-80 border border-gray-600 hover:border-gray-400 rounded-2xl p-10 shadow-2xl">
-          <Link
-            href="https://lmtreasuretrove.com"
-            target="_blank"
-            className="flex flex-col items-center"
-          >
-            <h2 className="text-center text-white text-xl">
-              LM Treasure Trove
-            </h2>
-            <img
-              src="/projects/lmtreasure.jpg"
-              className="h-32 border border-x-gray-600 border-y-gray-600 my-2"
-              alt="project image"
-            />
-            <div className="w-full flex flex-col gap-2 px-2">
-              <div className="flex flex-wrap gap-2 justify-center">
-                <div className="badge badge-primary">React</div>
-                <div className="badge badge-primary">Mongo DB</div>
-                <div className="badge badge-primary">Express</div>
-                <div className="badge badge-primary">Stripe API</div>
-              </div>
-              <div className="text-sm text-gray-500 ">
-                This project was built specific for a client and their currently
-                ran business. They needed a working cart that adds items. When
-                the client is ready to checkout the payment is processed through
-                Stripe. Fully functional Store.
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col gap-2 w-3/4 md:w-80 border border-gray-600 hover:border-gray-400 rounded-2xl p-10 shadow-2xl">
-          <Link
-            href="https://www.coupgroup.com"
-            target="_blank"
-            className="flex flex-col items-center"
-          >
-            <h2 className="text-center text-white text-xl">Coup Group</h2>
-            <img
-              src="/projects/coup.jpg"
-              className="h-32 border border-x-gray-600 border-y-gray-600 my-2"
-              alt="project image"
-            />
-            <div className="w-full flex flex-col gap-2 px-2">
-              <div className="flex flex-wrap gap-2 justify-center">
-                <div className="badge badge-primary">React</div>
-                <div className="badge badge-primary">Mongo DB</div>
-                <div className="badge badge-primary">Express</div>
-                <div className="badge badge-primary">Node</div>
-              </div>
-              <div className="text-sm text-gray-500 ">
-                This project was built for a client and changed to a SAAS. I had
-                close communication with the client on what was needed to help
-                manage their clients. Specifically, what information needs to be
-                stored and explaing how secure their clients data is.
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col gap-2 w-3/4 md:w-80 border border-gray-600 hover:border-gray-400 rounded-2xl p-10 shadow-2xl">
-          <Link
-            href="https://habiting.netlify.app/"
-            target="_blank"
-            className="flex flex-col items-center"
-          >
-            <h2 className="text-center text-white text-xl">Habiting</h2>
-            <img
-              src="/projects/habiting.jpg"
-              className="h-32 border border-x-gray-600 border-y-gray-600 my-2"
-              alt="project image"
-            />
-            <div className="w-full flex flex-col gap-2 px-2">
-              <div className="flex flex-wrap gap-2 justify-center">
-                <div className="badge badge-primary">React</div>
-                <div className="badge badge-primary">Supabase</div>
-                <div className="badge badge-primary">Oauth</div>
-              </div>
-              <div className="text-sm text-gray-500 ">
-                This project was a passion project for me. I wanted to build a
-                project that allowed me to track habits that I want to build. It
-                allows users to focus on a few core habits to build that muscle
-                to continue consistancy.
-              </div>
-            </div>
-          </Link>
-        </div>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
